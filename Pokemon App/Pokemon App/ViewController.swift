d5025f577d76de90079fc4c87aebd26142ba42e6
ScizorM <ml_martins@yahoo.com>
2020-09-09 13:53:23 -0300

    Create project

---
+++ Pokemon App/Pokemon App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokemon app
+//
+//  Created by Scizor on 09/09/20.
+//  Copyright © 2020 Scizor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
